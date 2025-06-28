3b7fc9b4f7743e98788c5069ca27f90ec90f9ab8
Cykalele <hauserthommy@web.de>
2025-06-28 19:34:50 +0200

    Automatischer Commit beim Start

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+import os
 import requests
-
+import streamlit as st
 # config.py
-API_BASE = "https://pest-berry-competitors-timothy.trycloudflare.com"
+#API_BASE = "https://pest-berry-competitors-timothy.trycloudflare.com"
 
 
 SESSION_ROOT = "../02_FootballAI/sessions"  # relativer Pfad zum Backend
+
+def get_api_base():
+    return st.secrets.get("API_BASE", os.getenv("API_BASE", "http://localhost:8000"))
