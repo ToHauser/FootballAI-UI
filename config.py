ebefbddd9341223f8315213e45bc376b8ad35e67
Cykalele <hauserthommy@web.de>
2025-06-28 13:27:08 +0200

    Automatischer Commit beim Start

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         print("⚠️ Keine Ngrok-URL gefunden.")
 
 # config.py
-API_BASE = "https://voltage-merchandise-strikes-john.trycloudflare.com"
+API_BASE = "https://equation-grenada-soonest-unlock.trycloudflare.com"
 
 
 SESSION_ROOT = "../02_FootballAI/sessions"  # relativer Pfad zum Backend
