381b8316676b8c96f8a0d3447e64117c0b7efedb
Cykalele <hauserthommy@web.de>
2025-06-28 15:01:41 +0200

    Automatischer Commit beim Start

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import requests
 
 # config.py
-API_BASE = "https://practical-code-phil-cb.trycloudflare.com"
+API_BASE = "https://observer-lace-bhutan-malta.trycloudflare.com"
 
 
 SESSION_ROOT = "../02_FootballAI/sessions"  # relativer Pfad zum Backend
