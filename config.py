d0981fa70052d5ada321dec9c335e19d22340fd4
Cykalele <hauserthommy@web.de>
2025-06-28 15:00:39 +0200

    🧹 Re-added .gitignore and removed pycache from index

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import requests
 
 # config.py
-API_BASE = "https://unity-antarctica-zone-crimes.trycloudflare.com"
+API_BASE = "https://practical-code-phil-cb.trycloudflare.com"
 
 
 SESSION_ROOT = "../02_FootballAI/sessions"  # relativer Pfad zum Backend
